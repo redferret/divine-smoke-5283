4e7c6b5ceb8f8b6bb86f49913d98e509b7f2ed28
Richard <rdesilvey@gmail.com>
2021-06-14 10:21:33 -0600

    Test for remove link

--- spec/features/gardens/plots/index_spec.rb
+++ spec/features/gardens/plots/index_spec.rb
@@ -36,6 +36,23 @@ RSpec.describe 'The index page for plots,' do
         end
       end
 
+      describe 'remove plant link,' do
+        it 'shows a button to remove the plant from the plot' do
+          within "#plant-#{@plant_1}" do
+            expect(page).to have_link('Remove Plant')
+            click_link 'Remove Plant'
+          end
+        end
+        
+        it 'navigates back to the index page without the plant shown' do
+          expect(current_path).to eq garden_plots_path(@garden)
+          
+          within "#plots-#{@plot_1}-plants-list" do
+            expect(page).to_not have_content(@plant_1.name)
+          end
+        end
+      end
+
       it 'under the plot number is the list of plants and their names' do
         within "#plots-#{@plot_1.id}-plants-list" do
           expect(page).to have_content(@plant_1.name)
