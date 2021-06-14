fa70df3dff77e718033da1078d8e842eee6bcd78
Richard <rdesilvey@gmail.com>
2021-06-14 11:02:04 -0600

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'application#index'
   
-  resources :gardens, only: [] do
+  resources :gardens, only: [ :index, :show ] do
     resources :plots, only: :index do
       resources :plants, only: [] do
         member do
