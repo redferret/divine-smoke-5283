acc60d1528a0cfb29a3e37b04fc635e00023ecbb
Richard <rdesilvey@gmail.com>
2021-06-14 10:15:10 -0600

    Write more tests

--- spec/features/gardens/plots/index_spec.rb
+++ spec/features/gardens/plots/index_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'The index page for plots,' do
   before :each do
     @garden = FactoryBot.create(:garden, name: 'My Garden')
+
     @plot_1 = FactoryBot.create(:plot, garden: @garden, number: 1)
     @plant_1 = FactoryBot.create(:plant)
     @plant_2 = FactoryBot.create(:plant)
@@ -13,7 +14,13 @@ RSpec.describe 'The index page for plots,' do
     @plant_4 = FactoryBot.create(:plant)
     @plot_2.plants << @plant_3 << @plant_4
 
-    visit plots_path
+    visit garden_plots_path(@garden)
+  end
+
+  describe 'page,' do
+    it 'has a title with the garden name' do
+      expect(page).to have_content(@garden.name)
+    end
   end
 
   describe 'main list,' do
@@ -23,15 +30,16 @@ RSpec.describe 'The index page for plots,' do
           within "#plot-#{@plot_1.id}" do
             expect(page).to have_content("Plot ##{@plot_1.number}")
           end
+          within "#plot-#{@plot_2.id}" do
+            expect(page).to have_content("Plot ##{@plot_2.number}")
+          end
         end
       end
 
       it 'under the plot number is the list of plants and their names' do
-        within "#plot-#{@plot_1.id}" do
-          within '#plants-list' do
-            expect(page).to have_content(@plant_1.name)
-            expect(page).to have_content(@plant_2.name)
-          end
+        within "#plots-#{@plot_1.id}-plants-list" do
+          expect(page).to have_content(@plant_1.name)
+          expect(page).to have_content(@plant_2.name)
         end
       end
     end
