d644d3d848a8f6744f6445ee2b0c084ac9b5356d
Richard <rdesilvey@gmail.com>
2021-06-14 11:44:01 -0600

    Write feature tests

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe 'The show page for a garden,' do
+  before :each do
+    @garden = FactoryBot.create(:garden, name: 'My Garden')
+
+    @plot_1 = FactoryBot.create(:plot, garden: @garden, number: 1)
+    @plant_1 = FactoryBot.create(:plant, days_til_harvest: 50, name: 'Squash')
+    @plant_2 = FactoryBot.create(:plant, days_til_harvest: 120)
+    @plot_1.plants << @plant_1 << @plant_2
+
+    @plot_2 = FactoryBot.create(:plot, garden: @garden, number: 2)
+    @plant_3 = FactoryBot.create(:plant, days_til_harvest: 45, name: 'Peppers')
+    @plot_2.plants << @plant_2 << @plant_3
+
+    visit garden_path(@garden)
+  end
+
   describe 'plants list,' do
-    it 'shows all the plants for this gardens plots'
+    it 'only shows distinct plant names' do
+      within '#plants-list' do
+        expect(page).to have_content('Squash', count: 1)
+        expect(page).to have_content('Peppers')
+      end
+    end
 
-    it 'only shows distinct plant names'
-
-    it 'only shows plants that are ready to harvest in less than 100 days'
+    it 'only shows plants that are ready to harvest in less than 100 days' do
+      within '#plants-list' do
+        expect(page).to have_content('50')
+        expect(page).to have_content('45')
+      end
+    end
   end
 end
