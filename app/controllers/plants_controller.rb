26771ce166c24254b28f697e18462b5e18d75e64
Richard <rdesilvey@gmail.com>
2021-06-14 10:44:21 -0600

    Implement feature

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,11 @@
 class PlantsController < ApplicationController
+  def remove_plant
+    @garden = Garden.find(params[:garden_id])
+
+    @plot = Plot.find(params[:plot_id])
+    @plant = @plot.plants.find(params[:id])
+    @plot.plants.delete(@plant)
+    
+    redirect_to garden_plots_path(@garden)
+  end
 end
