5b5b1229e6e2589f334c0580cce6c0bf3e9edf3c
Richard <rdesilvey@gmail.com>
2021-06-14 09:55:09 -0600

    Write feature tests for plots index

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -1,10 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'The index page for plots,' do
+  before :each do
+    @garden = FactoryBot.create(:garden, name: 'My Garden')
+    @plot_1 = FactoryBot.create(:plot, garden: @garden, number: 1)
+    @plant_1 = FactoryBot.create(:plant)
+    @plant_2 = FactoryBot.create(:plant)
+    @plot_1.plants << @plant_1 << @plant_2
+
+    @plot_2 = FactoryBot.create(:plot, garden: @garden, number: 2)
+    @plant_3 = FactoryBot.create(:plant)
+    @plant_4 = FactoryBot.create(:plant)
+    @plot_2.plants << @plant_3 << @plant_4
+
+    visit plots_path
+  end
+
   describe 'main list,' do
     describe 'each plot,' do
-      it 'has a number for that plot'
-      it 'under the plot number is the list of plants and their names'
+      it 'has a number for that plot' do
+        within '#plots-list' do
+          within "#plot-#{@plot_1.id}" do
+            expect(page).to have_content("Plot ##{@plot_1.number}")
+          end
+        end
+      end
+
+      it 'under the plot number is the list of plants and their names' do
+        within "#plot-#{@plot_1.id}" do
+          within '#plants-list' do
+            expect(page).to have_content(@plant_1.name)
+            expect(page).to have_content(@plant_2.name)
+          end
+        end
+      end
     end
   end
 end
