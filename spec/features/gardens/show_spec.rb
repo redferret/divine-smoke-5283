3930a7fdaa626a3531bda8627ba088df33c47761
Richard <rdesilvey@gmail.com>
2021-06-14 11:07:03 -0600

    Write expected feature tests for show page

---
+++ spec/features/gardens/show_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'The show page for a garden,' do
+  describe 'plants list,' do
+    it 'shows all the plants for this gardens plots'
+
+    it 'only shows distinct plant names'
+
+    it 'only shows plants that are ready to harvest in less than 100 days'
+  end
+end
