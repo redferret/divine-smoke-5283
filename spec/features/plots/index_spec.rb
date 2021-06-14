d318e54dee7a49bf90cf8cb42a01f32028ef0d29
Richard <rdesilvey@gmail.com>
2021-06-14 09:33:57 -0600

    Write expected feature tests

---
+++ spec/features/plots/index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'The index page for plots,' do
+  describe 'main list,' do
+    describe 'each plot,' do
+      it 'has a number for that plot'
+      it 'under the plot number is the list of plants and their names'
+    end
+  end
+end
